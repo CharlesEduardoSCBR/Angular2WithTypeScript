1ad456cb8ac35051134071219f0c50f18769dbde
CharlesEduardoSCBR <charles_eduardo_@hotmail.com>
2017-11-13 12:58:50 -0200

    3.x - directiva ngFor Arrow functions

--- client/app/app.component.ts
+++ client/app/app.component.ts
@@ -11,6 +11,11 @@ export class AppComponent {
     fotos : Object[] = [];
 
     constructor(http : Http){
-        let stream = http.get('v1/fotos');
+        http.get('v1/fotos')
+            .map(res => res.json())
+            .subscribe(
+            fotos => this.fotos = fotos,
+            erro => console.log(erro)
+            );
     }   
 }
