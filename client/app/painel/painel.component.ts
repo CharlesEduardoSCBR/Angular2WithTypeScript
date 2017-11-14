2ec2164649c710e4304599b72b4f0eaadd728cb8
CharlesEduardoSCBR <charles_eduardo_@hotmail.com>
2017-11-13 22:48:45 -0200

    5.1 - Adicionando o painel compoent

---
+++ client/app/painel/painel.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+    selector: 'painel',
+    templateUrl: './app/painel/painel.component.html'
+})
+
+export class PainelComponent {
+
+    @Input() titulo : string;
+}
