11d84fbfd161d37f59219372eddeb12e456ac91c
CharlesEduardoSCBR <charles_eduardo_@hotmail.com>
2017-11-13 23:28:59 -0200

    4.3 - Adicionando rotas

--- client/app/app.module.ts
+++ client/app/app.module.ts
@@ -7,9 +7,10 @@ import { HttpModule } from '@angular/http';
 import { PainelModule } from './painel/painel.module';
 import { CadastroComponent } from "./cadastro/cadastro.component";
 import { ListagemComponent } from "./listagem/listagem.component";
+import {routing } from './app.route'
 
 @NgModule({
-    imports:        [ BrowserModule , FotoModule, HttpModule, PainelModule ],
+    imports:        [ BrowserModule , FotoModule, HttpModule, PainelModule, routing ],
     declarations:   [ AppComponent, CadastroComponent, ListagemComponent ],
     bootstrap:      [ AppComponent ]
 })
